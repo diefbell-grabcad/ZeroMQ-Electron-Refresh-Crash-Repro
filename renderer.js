45de6a727fe62439e8397f1c8b01e11ae3f00c9f
Dief Bell <dief.bell@grabcad.com>
2024-11-13 14:50:41 +0000

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,16 @@
+import * as zmq from "zeromq/v5-compat"
+
+const socket = zmq.createSocket("dealer");
+socket.pause();
+
+socket.on("connect", (fd, ep) => {
+    console.log("Connected");
+    socket.resume();
+});
+
+socket.on("disconnect", (fd, ep) => {
+    console.log("Disconnected")
+    socket.pause();
+});
+
+
