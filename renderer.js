aa0c7cc80b1f5de28c8320299ec5275f3179af2c
Dief Bell <dief.bell@grabcad.com>
2024-11-15 11:35:31 +0000

    Not even creating a socket, still crashes

--- renderer.js
+++ renderer.js
@@ -1,14 +1,14 @@
 const { ipcRenderer } = require("electron");
 const zmq = require("zeromq/v5-compat");
 
-const socket = zmq.createSocket("dealer");
+// const socket = zmq.createSocket("dealer");
 
 ipcRenderer.on("my-channel", (event, action) => {
     console.log(`Received IPC message from main process. Action: "${action}"`)
     switch(action) {
         case "reload":
-            socket.close();
-            console.log("Socket closed...");
+            // socket.close();
+            console.log("We'd close the socket here, if it was even open in the first place!");
             break;
         default:
             console.error("Unknown action:", action);
